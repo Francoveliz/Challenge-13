b42ee8cce762bdfefc9debf7f463f4864bc0a07c
Francoveliz <francoleandroveliz@gmail.com>
2020-07-13 05:02:12 -0300

    mobile menu

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 const menuBtn = document.querySelector(".menu-btn");
+const navBar = document.getElementById("nav-bar");
+const headerBar = document.querySelector("body");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
+    /* nav animation */
+    navBar.classList.toggle("nav-active");
+    /* hamburger button animation */
     if (!menuOpen) {
         menuBtn.classList.add("open");
+        headerBar.style.position = "fixed";
         menuOpen = true;
     } else {
         menuBtn.classList.remove("open");
+        headerBar.style.position = "static";
         menuOpen = false;
     }
 });
