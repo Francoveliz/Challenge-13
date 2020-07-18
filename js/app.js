1508d53b6b29b775c32d4b9aa1df7ae7477cd9f8
Francoveliz <francoleandroveliz@gmail.com>
2020-07-18 03:11:55 -0300

    challenge completed

--- js/app.js
+++ js/app.js
@@ -4,24 +4,24 @@ let menuOpen = false;
 
 /* disable body scroll function */
 function noScroll() {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 }
 
 menuBtn.addEventListener("click", () => {
-    /* nav animation */
-    navBar.classList.toggle("nav-active");
-    /* hamburger button animation */
-    if (!menuOpen) {
-        // listener to disable scroll
-        window.addEventListener('scroll', noScroll);
-        menuBtn.classList.add("open");
+  /* nav animation */
+  navBar.classList.toggle("nav-active");
+  /* hamburger button animation */
+  if (!menuOpen) {
+    // listener to disable scroll
+    window.addEventListener("scroll", noScroll);
+    menuBtn.classList.add("open");
 
-        menuOpen = true;
-    } else {
-        /* close hamburger menu */
-        menuBtn.classList.remove("open");
-        menuOpen = false;
-        // enable scroll
-        window.removeEventListener('scroll', noScroll);
-    }
+    menuOpen = true;
+  } else {
+    /* close hamburger menu */
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    // enable scroll
+    window.removeEventListener("scroll", noScroll);
+  }
 });
